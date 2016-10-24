aa286d361126c8b0d3aac38deb017ef2a8067846
Helimyne <helimyne@gmail.com>
2016-10-24 13:49:03 -0700

    Intellij setup, client and server running!

--- src/main/java/ca/helimyne/minechem/Minechem.java
+++ src/main/java/ca/helimyne/minechem/Minechem.java
@@ -1,20 +1,3 @@
-package com.example.examplemod;
+package ca.helimyne.minechem;
 
-import net.minecraft.init.Blocks;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
-    }
-}
+public class Minechem{}
